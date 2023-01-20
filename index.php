659d88c531f05f7ec664208014c2e5a0af393bc5
Eduardo Meneghel <eduardopedromeneghel@outlook.com.br>
2023-01-19 22:23:01 -0300

    feat/adicionado primeiros arquivos css, index e imagem para test

---
+++ index.php
@@ -0,0 +1,14 @@
+<?php
+require_once './vendor/autoload.php';
+
+$loader = new \Twig\Loader\FilesystemLoader('view');
+$twig = new \Twig\Environment($loader);
+
+$data = [];
+$data['info'] = 'A molécula é formada por um grupamento de dois ou mais átomos, unidos por ligações covalentes. Ela ainda apresenta duas características básicas: estabilidade (ligações difíceis de serem desfeitas) e neutralidade elétrica (mesmo número de prótons e elétrons)';
+$data['title'] = 'Data';
+$data['loja'] = 'Data';
+
+echo $twig->render('index.html', $data);
+
+?>
