d42df911a0b3c76beefbd7a6298e04f42c405e82
Eduardo Meneghel <eduardopedromeneghel@outlook.com.br>
2023-02-09 23:26:12 -0300

    refactor/corrigindo erros de chamadas de arquivos por conta do novo padrão de projeto

--- public/admin.php
+++ public/admin.php
@@ -4,6 +4,6 @@ require_once '../vendor/autoload.php';
 $loader = new \Twig\Loader\FilesystemLoader('../app/views/admin');
 $twig = new \Twig\Environment($loader);
 
-echo $twig->render('index.html');
+echo $twig->render('/login.html');
 
 ?>
--- public/validateLogin.php
+++ public/validateLogin.php
@@ -1,6 +1,7 @@
 <?php
     if($_POST["username"] == 'login' && $_POST["password"] == '123') {
-        header('Location: /website-manager/admin/home.php');
+        header("Location: /website-manager/public/home.php");
+        die();
     }else{
         echo "user or password invalid";
     }
