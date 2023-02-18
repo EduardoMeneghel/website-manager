aa4adea3d24aa9a8ec892e69d826a9e93216517f
Eduardo Meneghel <eduardopedromeneghel@outlook.com.br>
2023-02-18 19:00:04 -0300

    feat/adicionado validação para recuperação de conta

--- admin/validateForgotPassword.php
+++ admin/validateForgotPassword.php
@@ -1,5 +1,10 @@
 <?php
 require_once('../app/models/QueryManager.php');
 $queryManager = new QueryManager;
-var_dump($_POST);
+$row = $queryManager->executeQuery('SELECT * FROM user WHERE ds_email = '.'"'.$_POST["email"].'"');
+if($row){
+    echo "<script>alert('check your email!');</script>";
+} else {
+    echo "<script>alert('check your email.');</script>";
+}
 ?>
