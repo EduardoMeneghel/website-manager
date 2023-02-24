42d3c0bfd191f9e85d873151b9f5d995b14c93cc
Eduardo Meneghel <eduardopedromeneghel@outlook.com.br>
2023-02-23 23:01:56 -0300

    feat/alterando tabela, resolvendo erro de query e imprimindo na tela

--- admin/home.php
+++ admin/home.php
@@ -1,4 +1,5 @@
 <?php
+
 require_once '../vendor/autoload.php';
 use App\Models\QueryManager;
 use Twig\Loader\FilesystemLoader;
@@ -8,7 +9,7 @@ $loader = new FilesystemLoader('../app/views/admin');
 $twig = new Environment($loader);
 
 $queryManager = new QueryManager;
-$rows = $queryManager->executeQuery('SELECT * FROM menu_admin_itens as a RIGHT JOIN menu_admin as b ON a.id_menu_admin =  b.id ORDER BY b.ds_ordination ASC');
+$rows = $queryManager->executeQuery('SELECT p.*, (SELECT COALESCE(CONCAT("[", GROUP_CONCAT(JSON_OBJECT("id_menu_admin", f.id_menu_admin, "nm_name_child", f.nm_name_child, "ds_url_child", f.ds_url_child, "ds_ordination_child", f.ds_ordination_child)), "]"), "[]") FROM menu_admin_itens f WHERE f.id_menu_admin =  p.id ) AS children FROM menu_admin p ORDER BY p.ds_ordination ASC');
 echo $twig->render('index.html', array('menu_admin' => $rows));
 
 ?>
